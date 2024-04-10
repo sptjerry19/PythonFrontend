db223010bfe9e5baf2b541f09facd142b85fd63d
sptjerry19 <duylinhvnu@gmail.com>
2024-04-10 15:53:02 +0700

    init project

---
+++ src/store.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { createStore } from "vuex";
+
+// Create a new store instance.
+const store = createStore({
+  state: {
+    UrlServe: "http://127.0.0.1:8000/api/cms",
+    urlStorage: "http://127.0.0.1:8000/storage/",
+    token: localStorage.getItem("token"),
+    isAuthenticated: false,
+  },
+  getters: {
+    doneLogin(state) {
+      return state.tokenUser.length > 20
+        ? (state.isLogin = true)
+        : (state.isLogin = false);
+    },
+    getToken: (state) => state.token,
+  },
+  actions: {
+    saveToken({ commit }, token) {
+      commit("setToken", token);
+    },
+  },
+  mutations: {
+    setToken(state, token) {
+      state.token = token;
+    },
+  },
+});
+
+export default store;
